eeb1406f7d0ba94119cf2b23ef4e6fd65374767d
Henry <hennadii.lytvyn@gmail.com>
2025-06-03 21:39:47 +0100

    nav button work

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,13 +1,16 @@
 const mobileBtn = document.querySelector('[data-id="mobile-btn"]');
 const mobileMenu = document.querySelector('[data-id="mobile-menu"]');
 const navLinks = document.querySelectorAll('[data-id="nav-link"]');
+const navIcon = document.querySelector('[data-id="nav-icon"]');
 
 mobileBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
+  navIcon.classList.toggle('open');
 });
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     mobileMenu.classList.remove('active');
+    navIcon.classList.remove('open');
   });
 });
